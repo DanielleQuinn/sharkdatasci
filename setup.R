80abb695d3d7686fe03eaf0dbfaf3d6a5298897c
DanielleQuinn <danielle.quinn@mun.ca>
2020-07-14 20:08:58 -0230

    updated data, set factor levels, adjusted colours

--- setup.R
+++ setup.R
@@ -2,13 +2,28 @@
 library(tidyverse)
 
 # Data Import / Modification
-data <- read.csv("sharkdata.csv")
-floordata <- data %>%
+rawdata <- read.csv("sharkdata.csv")
+floordata <- rawdata %>%
   arrange(desc(depth)) %>%
   mutate(rank = 1:nrow(.)) %>%
   dplyr::select(rank, depth) %>%
   add_row(rank = max(.$rank), depth = 1500, .before = 1)
-data <- left_join(data, floordata)
-shark_data <- data %>% select(-1, -rank)
+rawdata <- left_join(rawdata, floordata)
+data <- rawdata %>%
+  select(-1, -rank) %>%
+  mutate(species = factor(species, c("Deep Water Catshark",
+                                     "Greenland Shark",
+                                     "Spiny Dogfish",
+                                     "Barndoor Skate")))
 
-# Function Creation
+
+# Build Functions
+bargraph_sharks <- function(x = 12)
+{
+  bargraph1 <<- ggplot(data) +
+    geom_bar(aes(x = species, fill = species), col = "black") +
+    scale_fill_manual(values = c("darkorange", "purple", "olivedrab3", "firebrick3")) +
+    theme_bw(x) + ylab("Count") + xlab("") +
+    theme(legend.position = "none")
+  return(bargraph1)
+}
