ec860725217f67249518b190c0c65ce9ef87388c
DanielleQuinn <danielle.quinn@mun.ca>
2020-07-14 19:53:02 -0230

    created setup.R for data import, prep, and package loading

---
+++ setup.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+data <- read.csv("sharkdata.csv")
+floordata <- data %>%
+  arrange(desc(depth)) %>%
+  mutate(rank = 1:nrow(.)) %>%
+  dplyr::select(rank, depth) %>%
+  add_row(rank = max(.$rank), depth = 1500, .before = 1)
+data <- left_join(data, floordata)
+shark_data <- data %>% select(-1, -rank)
