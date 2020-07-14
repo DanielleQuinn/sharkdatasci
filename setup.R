0ea54c67f31be363c21d6db676abfac03562d187
DanielleQuinn <danielle.quinn@mun.ca>
2020-07-14 19:54:17 -0230

    Added sections

--- setup.R
+++ setup.R
@@ -1,4 +1,7 @@
+# Load Packages
 library(tidyverse)
+
+# Data Import / Modification
 data <- read.csv("sharkdata.csv")
 floordata <- data %>%
   arrange(desc(depth)) %>%
@@ -7,3 +10,5 @@ floordata <- data %>%
   add_row(rank = max(.$rank), depth = 1500, .before = 1)
 data <- left_join(data, floordata)
 shark_data <- data %>% select(-1, -rank)
+
+# Function Creation
